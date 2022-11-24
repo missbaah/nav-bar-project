a9cf8116f8da951f846981681184b019203223f4
Adwoa Addo-Brako <baahbrako@gmail.com>
2022-11-24 22:52:51 +0000

    adding toggle functionality

--- app.js
+++ app.js
@@ -3,3 +3,17 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const toggleBtn = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+// console.log(toggleBtn);
+
+toggleBtn.addEventListener("click", (e) => {
+  // checking if the links element contains the class "show-links"
+  if (links.classList.contains("show-links")) {
+    links.classList.remove("show-links");
+  } else {
+    links.classList.add("show-links");
+  }
+});
